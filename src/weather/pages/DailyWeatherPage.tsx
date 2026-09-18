67325e3f78c0d2bae0482d0fc97b2f055f220ea6
ArsenKarapetyan03 <karapetyanarsen224@gmail.com>
2026-09-18 17:48:59 +0400

    smth

--- src/weather/pages/DailyWeatherPage.tsx
+++ src/weather/pages/DailyWeatherPage.tsx
@@ -1,10 +1,10 @@
-import type { WeatherData } from "../model/weather.types.ts";
+import type { Weather, WeatherData } from "../model/weather.types.ts";
 
 
 export const DailyWeatherPage = ({dailyWeatherData}: {dailyWeatherData: WeatherData}) => {
 
 
-
+	// console.log(getDailyMaxWeather(dailyWeatherData))
 	return (
 		<div
 			className="flex flex-col gap-2 p-3 text-2xl bg-black/5 shadow-[5px_5px_20px_rgba(0,0,0,0.25)] rounded-lg">
@@ -14,7 +14,7 @@ export const DailyWeatherPage = ({dailyWeatherData}: {dailyWeatherData: WeatherD
 						key={item.dt}
 						className="text-blue-100 font-bold text-left"
 					>
-						{/*{new Date(Number(item.dt)*1000).getDate()}*/}
+
 					</div>
 				)
 			})}
@@ -22,4 +22,20 @@ export const DailyWeatherPage = ({dailyWeatherData}: {dailyWeatherData: WeatherD
 			{dailyWeatherData.list.length}
 		</div>
 	)
+}
+
+
+function getDailyMaxWeather(data: WeatherData): Weather[] {
+	const dailyGroups: Record<string, Weather> = {};
+
+	data.list.forEach((item) => {
+		const date = item.dt.split(' ')[0];
+		const currentTemp = item.main.temp;
+
+		if (!dailyGroups[date] || currentTemp > dailyGroups[date].main.temp) {
+			dailyGroups[date] = item;
+		}
+	});
+
+	return Object.values(dailyGroups);
 }
