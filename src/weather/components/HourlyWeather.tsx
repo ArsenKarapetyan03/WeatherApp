40a4db0c0fd95ea9c60d5ad08e51808a15e2af93
ArsenKarapetyan03 <karapetyanarsen224@gmail.com>
2026-09-18 12:32:49 +0400

    ....

--- src/weather/components/HourlyWeather.tsx
+++ src/weather/components/HourlyWeather.tsx
@@ -8,10 +8,8 @@ export const HourlyWeather = ({weatherData}: {weatherData: WeatherData}) => {
 		<div className="flex flex-col gap-2 my-20 p-3 mx-auto max-w-2/3 text-2xl shadow-[5px_5px_15px_rgba(0,0,0,0.3)] rounded-lg">
 			<div className="flex justify-evenly">
 				{/* Temperature */}
-				<div className="flex justify-center">
-					<div className="text-9xl text-white font-bold">{Math.round(main.temp)}</div>
-					<span className="text-3xl text-blue-100 font-bold">o</span>
-					<span className="text-5xl text-blue-100 font-bold pt-4">C</span>
+				<div className="flex justify-center font-semibold text-blue-50">
+					<div>Now {Math.round(main.temp)}</div>
 				</div>
 
 			</div>
