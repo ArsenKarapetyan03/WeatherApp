ea52da9591d542fd861e81bf6f904fd16bace67a
ArsenKarapetyan03 <karapetyanarsen224@gmail.com>
2026-09-22 10:15:22 +0400

    ...

--- src/weather/components/DailyWeather.tsx
+++ src/weather/components/DailyWeather.tsx
@@ -1,8 +1,7 @@
 import { CalendarDays, MoveDown, MoveUp } from "lucide-react";
 import type { Weather, WeatherData } from "../model/weather.types.ts";
 import { ICON_URL } from "../model/weather.config.ts";
-import {CollapsiblePanel} from "custom-ui-components/src/components/CollapsiblePanel.tsx";
-import {cn} from "custom-ui-components/src/lib/utils.ts";
+import { CollapsiblePanel } from "custom-ui-components/src/components/CollapsiblePanel.tsx";
 
 export const DailyWeather = ({dailyWeatherData}: { dailyWeatherData: WeatherData }) => {
 
@@ -11,18 +10,20 @@ export const DailyWeather = ({dailyWeatherData}: { dailyWeatherData: WeatherData
 			title:
 				(<div
 					key={item.dt}
-					className="flex justify-between text-blue-100 font-bold text-left"
+					className="w-full flex justify-between text-blue-100 font-bold"
 				>
 					<div>{new Date(Number(item.dt) * 1000).toLocaleDateString('en-US', {weekday: 'long'})}</div>
-					<div className="flex justify-between gap-2">
+					<div className="flex gap-15">
 						<img src={ICON_URL + item.weather[0].icon.replace('n', 'd') + ".png"} alt=""/>
-						<div className="flex items-center">
-							<MoveDown strokeWidth={3} size={20} className="text-blue-200" />
-							{Math.round(item.main.temp_min)}°
-						</div>
-						<div className="flex items-center">
-							<MoveUp strokeWidth={3} size={20} className="text-blue-200" />
-							{Math.round(item.main.temp_max)}°
+						<div className="flex gap-5">
+							<div className="flex items-center">
+								<MoveDown strokeWidth={3} size={20} className="text-blue-200"/>
+								{Math.round(item.main.temp_min)}°
+							</div>
+							<div className="flex items-center">
+								<MoveUp strokeWidth={3} size={20} className="text-blue-200"/>
+								{Math.round(item.main.temp_max)}°
+							</div>
 						</div>
 					</div>
 				</div>),
@@ -31,7 +32,8 @@ export const DailyWeather = ({dailyWeatherData}: { dailyWeatherData: WeatherData
 	});
 
 	return (
-		<div className="mb-4 flex flex-col gap-2 p-3 text-2xl bg-black/10 shadow-[5px_5px_20px_rgba(0,0,0,0.25)] rounded-lg">
+		<div
+			className="mb-4 flex flex-col gap-2 p-3 text-2xl bg-black/10 shadow-[5px_5px_20px_rgba(0,0,0,0.25)] rounded-lg">
 			<div className="font-bold text-3xl text-blue-200">
 				<CalendarDays className="inline"/>
 				<span> 5-day forecast</span>
@@ -40,8 +42,9 @@ export const DailyWeather = ({dailyWeatherData}: { dailyWeatherData: WeatherData
 			<CollapsiblePanel
 				panels={panels}
 				containerStyles="border-none"
-				titleStyles="bg-transparent border-b border-gray-200"
+				titleStyles="w-full bg-transparent border-b border-gray-200"
 				contentStyles="border-none"
+				icon={false}
 			/>
 
 			<div className="divide-y">
